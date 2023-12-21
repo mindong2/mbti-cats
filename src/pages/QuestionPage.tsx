a7a45ae818ea75afa6d42d1d371048b31d7346d9
mindong2 <qlsnaos12@naver.com>
2023-12-21 11:57:34 +0900

    질문페이지 테스트로직 완성

--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -33,22 +33,52 @@ const QuestionWrap = styled.div`
 const QuestionPage = () => {
   const navigate = useNavigate();
   const [questionNum, setQuestionNum] = useState(0);
-  const selectQuestion = () => {
+  const [totalScore, setTotalScore] = useState([
+    {
+      type: "EI",
+      score: 0,
+    },
+    {
+      type: "SN",
+      score: 0,
+    },
+    {
+      type: "TF",
+      score: 0,
+    },
+    {
+      type: "JP",
+      score: 0,
+    },
+  ]);
+
+  const selectQuestion = (type: string, ans: number) => {
+    // 선택할 때 마다 totalScore 갱신
+    const addScore = totalScore.map((item) => {
+      if (type === item.type) {
+        item.score += ans;
+      }
+      return item;
+    });
+    setTotalScore(addScore);
+
+    // 마지막 질문이 아닐경우
     if (questionNum === QuestionData.length - 1) {
       navigate("/result");
     }
     setQuestionNum((prev) => prev + 1);
   };
+
   return (
     <>
       <Header type="progress" questionNum={questionNum} />
       <Wrapper>
         <QuestionTitle>{QuestionData[questionNum].title}</QuestionTitle>
         <QuestionWrap>
-          <Button className="questionBtn" onClick={selectQuestion}>
+          <Button className="questionBtn" onClick={() => selectQuestion(`${QuestionData[questionNum].type}`, 1)}>
             {QuestionData[questionNum].answerA}
           </Button>
-          <Button className="questionBtn" onClick={selectQuestion}>
+          <Button className="questionBtn" onClick={() => selectQuestion(`${QuestionData[questionNum].type}`, 0)}>
             {QuestionData[questionNum].answerB}
           </Button>
         </QuestionWrap>
